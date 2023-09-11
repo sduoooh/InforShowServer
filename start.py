4a388c950a32753fc7b8444970eb38312893d8ab
sduoooh <3101522606@qq.com>
2023-09-12 00:06:46 +0800

    .

--- start.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-
-def start(address: str):
-    proc = subprocess.Popen(['', 'bar.py', None], shell=True)
-
-start("cd C:/Users/1/Desktop/program/InforShowServer1/qq/3101522606 && go-cqhttp_windows_amd64.exe")
